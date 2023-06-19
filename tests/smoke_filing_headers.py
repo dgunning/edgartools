4aca9fdfc2c11c90548686ffceda8460afe1256c
Dwight Gunning <dgunning@gmail.com>
2023-06-19 08:58:21 -0400

    Add smoke test for filing_headers

---
+++ tests/smoke_filing_headers.py
@@ -0,0 +1,12 @@
+
+from edgar import *
+from rich import print
+from rich.text import Text
+if __name__ == '__main__':
+    set_identity('Mark Townsend marktownsend@towns.com')
+    for filing in get_filings().sample(100):
+        try:
+            print(Text(filing.header.header_text))
+            print(filing.header)
+        except Exception as e:
+            print(f"Failed to get header for {filing.accession_no} {e}")
