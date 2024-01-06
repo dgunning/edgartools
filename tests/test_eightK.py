5fee523851d47ea3ccc5e13b8cade0e88df8aff1
Dwight Gunning <dgunning@gmail.com>
2024-01-06 10:28:13 -0500

    Minor code cleanup

--- tests/test_eightK.py
+++ tests/test_eightK.py
@@ -1,5 +1,7 @@
 from edgar import Filing
 from edgar.company_reports import EightK
+from unstructured.partition.html import partition_html
+from pathlib import Path
 
 
 def test_eightk_repr():
@@ -8,3 +10,22 @@ def test_eightk_repr():
     eightk: EightK = filing.obj()
     print()
     print(eightk)
+
+def test_eightk_item_parsing_after_dollar_sign():
+    # This issue was reported ib https://github.com/dgunning/edgartools/issues/21
+    # The issue was that the text in item 8.01 was cutoff after the dollar sign
+    filing = Filing(company='NexPoint Capital, Inc.', cik=1588272, form='8-K', filing_date='2023-12-20',
+                    accession_no='0001193125-23-300021')
+
+    with Path('data/Nextpoint.8k.html').open('w') as f:
+        f.write(filing.html())
+    # partition the html and test if the text is in one of the elements
+    elements = partition_html(text=filing.html())
+    for element in elements:
+        print(element.text)
+
+    #text = filing.text()
+    #eightk = filing.obj()
+    #item_801 = eightk['Item 8.01']
+    #print(item_801)
+    #print(text)
