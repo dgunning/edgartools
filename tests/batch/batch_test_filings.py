5b8dde2467fce9b6d8b2420ceddd82b94b9f416e
Dwight Gunning <dgunning@gmail.com>
2024-03-05 13:45:16 -0500

    Update batch tests

---
+++ tests/batch/batch_test_filings.py
@@ -0,0 +1,20 @@
+from tqdm import tqdm
+from edgar import get_filings
+
+
+def view_filing_text():
+    for filing in tqdm(get_filings().sample(100)):
+        try:
+            filing.text()
+            filing.view()
+            filing.markdown()
+            filing.obj()
+        except Exception as e:
+            print(f"Failed to get text for {filing}")
+            filing.open()
+            print(e)
+            raise
+
+
+if __name__ == '__main__':
+    view_filing_text()
