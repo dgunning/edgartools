0530808baa76844c7915f146166c499612a70e11
Dwight Gunning <dgunning@gmail.com>
2024-04-02 22:05:38 -0400

    Change batch size

--- tests/batch/batch_test_filings.py
+++ tests/batch/batch_test_filings.py
@@ -3,7 +3,7 @@ from edgar import get_filings
 
 
 def view_filing_text():
-    for filing in tqdm(get_filings().sample(100)):
+    for filing in tqdm(get_filings(year=2024).sample(200)):
         try:
             filing.text()
             filing.view()
