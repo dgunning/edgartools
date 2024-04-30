54f96726d6a3156e42da6bf0e2cab28a66e91114
Dwight Gunning <dgunning@gmail.com>
2024-04-30 11:21:42 -0400

    Change to accept parameters

--- tests/batch/batch_filings.py
+++ tests/batch/batch_filings.py
@@ -1,12 +1,15 @@
+import argparse
+
 from tqdm import tqdm
-from edgar import get_filings, Filing
+
+from edgar import get_filings
 
 
-def inspect_filings():
-    #for filing in tqdm(get_filings(year=[1996,1997,1998]).sample(200)):
-    for filing in tqdm(get_filings(year=2024).sample(200)):
+def sample_and_test_filings(years, sample_size):
+    print(f"Sampling {sample_size} filings from {years}")
+    for filing in tqdm(get_filings(year=years).sample(sample_size)):
         try:
-            filing.attachments
+            attachments = filing.attachments
             filing.text()
             filing.view()
             filing.markdown()
@@ -19,5 +22,16 @@ def inspect_filings():
             raise
 
 
+def main():
+    parser = argparse.ArgumentParser(description='Inspect filings from specific years with a given sample size.')
+    parser.add_argument('-y', '--years', type=int, nargs='+', required=True,
+                        help='Years to fetch filings for (e.g., -y 1999 2000)')
+    parser.add_argument('-s', '--sample_size', type=int, required=True,
+                        help='Sample size of filings to process')
+
+    args = parser.parse_args()
+    sample_and_test_filings(args.years, args.sample_size)
+
+
 if __name__ == '__main__':
-    inspect_filings()
+    main()
