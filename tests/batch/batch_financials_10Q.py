495dbef456fa37064d0719613f17fe98ce58b39a
Dwight Gunning <dgunning@gmail.com>
2024-08-13 15:48:54 -0400

    Fix imports in batch financials

--- tests/batch/batch_financials_10Q.py
+++ tests/batch/batch_financials_10Q.py
@@ -1,5 +1,6 @@
 from edgar import *
-from edgar.xbrl import XBRLData, XBRLInstance, Statement, Statements, Financials
+from edgar.xbrl import XBRLData, get_xbrl_object
+from edgar.financials import Financials
 import time
 from rich import print as rprint
 
@@ -7,11 +8,12 @@ def run_tenq_financials(num):
     filings = get_filings(form='10-Q').sample(num)
     for filing in filings:
         print(str(filing))
-        xbrl_data = XBRLData.extract(filing)
-        financials = Financials(xbrl_data)
-        income_statement = financials.get_income_statement()
-        rprint(income_statement)
-        time.sleep(13)
+        xbrl_data = get_xbrl_object(filing)
+        if xbrl_data:
+            financials = Financials(xbrl_data)
+            income_statement = financials.get_income_statement()
+            rprint(income_statement)
+            time.sleep(3)
 
 
 if __name__ == '__main__':
