15c37907e0a26f07b2651bf8909fc9b3b6161c68
Dwight Gunning <dgunning@gmail.com>
2025-01-18 08:17:37 -0500

    Add new Company package

---
+++ edgar/company/__init__.py
@@ -0,0 +1,10 @@
+from edgar.entities import *
+from edgar.reference.tickers import get_cik_tickers
+from typing import Iterable
+
+def public_companies() -> Iterable[Company]:
+    """
+    Iterate over all companies in the CIK tickers dataset
+    """
+    for row in get_cik_tickers().itertuples():
+        yield Company(row.cik)
