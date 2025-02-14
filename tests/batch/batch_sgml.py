b51ccf5431b3fbbe689bcb905aeede9c96632d7c
Dwight Gunning <dgunning@gmail.com>
2025-02-14 05:18:49 -0500

    More section tags for the SGML parser. Add batch test on local SGML files for '2015-02-13' and '2025-02-13'

--- tests/batch/batch_sgml.py
+++ tests/batch/batch_sgml.py
@@ -2,22 +2,24 @@ import argparse
 
 from tqdm import tqdm
 
-from edgar import get_filings
+from edgar import get_filings, use_local_storage
 
 
 
 def main():
-    filings = get_filings(year=[2000, 2005, 2010, 2014, 2018, 2022, 2025]).sample(1000)
+    # '2015-02-13', '2025-02-13' These dates are downloaded locally
+    filings = get_filings(filing_date='2015-02-13')
     for filing in tqdm(filings):
         try:
             #attachments = filing.attachments
             filing.sgml()
         except Exception as e:
-            print(f"Failed to get SGML attachments for {filing}")
+            print(f"Failed to get SGML for {filing}")
             filing.open()
             print(e)
             raise
 
 
 if __name__ == '__main__':
+    use_local_storage(True)
     main()
