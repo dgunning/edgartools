a92d8db4bcec23917c1d7c48fd6fcad7cbc1bf72
Dwight Gunning <dgunning@gmail.com>
2025-03-01 21:54:43 -0500

    Add tests for entity filings

---
+++ tests/test_entity_filings.py
@@ -0,0 +1,22 @@
+from edgar import *
+
+
+def test_equality_of_filings():
+    c = Company("AAPL")
+    filings = c.get_filings().filter(form="10-K")
+
+    filings2 = c.filings.filter(form="10-K")
+
+    assert filings == filings2
+    filings2 = filings2.filter(filing_date="2023-01-01:2024-12-31")
+    assert filings != filings2
+
+    filings3 = c.filings.filter(form="10-Q")
+    assert filings != filings3
+
+
+def test_filings_hash():
+    c = Company("AAPL")
+    filings = c.filings
+    _hash = hash(filings)
+    assert _hash
