48b4d62e8f6e28d3aac8a77cde3c7ce596fcd8e9
Dwight Gunning <dgunning@gmail.com>
2025-03-02 14:51:14 -0500

    Tests for - add derivative transactions to transaction summary

---
+++ tests/test_form4.py
@@ -0,0 +1,33 @@
+from edgar.ownership import Ownership, Form4, TransactionSummary
+from edgar import *
+
+
+def test_form4_with_derivatives():
+    filing = Filing(company='VERTEX PHARMACEUTICALS INC / MA',
+                    cik=875320,
+                    form='4',
+                    filing_date='2020-10-19',
+                    accession_no='0001209191-20-055264')
+    form4:Form4 = filing.obj()
+    print()
+    _repr = repr(form4)
+    assert _repr
+    activities = form4.get_transaction_activities()
+    assert len(activities) == 1
+    activity = activities[0]
+    assert activity.transaction_type == 'derivative_purchase'
+    assert activity.value == 43750.08036
+    assert activity.shares == 196.356
+    assert activity.security_title == 'Deferred Stock Units'
+    assert activity.underlying_security == 'Common Stock'
+
+    # Data
+    df = form4.to_dataframe()
+    print(df)
+
+def test_create_form4_with_non_numeric_underlying():
+    filing = Filing(form='4', filing_date='2024-10-02', company='Arnaboldi Nicole S', cik=1950032, accession_no='0001062993-24-017232')
+    form4:Form4 = filing.obj()
+    assert form4
+    print()
+    print(form4)
