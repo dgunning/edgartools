c5cba8350868d5a75592f358aff2bda2e69ce33c
Dwight Gunning <dgunning@gmail.com>
2025-03-08 21:14:11 -0500

    Add more details to batch_insiders test

--- tests/batch/batch_insiders.py
+++ tests/batch/batch_insiders.py
@@ -6,6 +6,8 @@ if __name__ == '__main__':
     for filing in tqdm(get_filings(form=[3,4,5], year=2024).sample(200)):
         try:
             ownership:Ownership = filing.obj()
+            df = ownership.to_dataframe()
+            df2 = ownership.to_dataframe(detailed=False)
             print()
             print(ownership)
         except Exception as e:
