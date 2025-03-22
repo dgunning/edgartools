0ca0684ba57e1070e4d09afb589fbebdc57920c7
Dwight Gunning <dgunning@gmail.com>
2025-03-21 21:51:57 -0400

    Add __init__.py to xbrl2/data package to have the directory treated as a python package

---
+++ edgar/xbrl2/data/__init__.py
@@ -0,0 +1,6 @@
+"""
+Data files for XBRL2 module.
+
+This package contains data files used by the XBRL2 module, including:
+- concept_mappings.json: Mappings between company-specific XBRL concepts and standardized concepts
+"""
