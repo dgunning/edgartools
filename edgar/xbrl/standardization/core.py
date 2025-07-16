df1864de1aa621345bc36ff340cf68398320e0e3
Dwight Gunning <dgunning@gmail.com>
2025-07-16 18:45:46 -0400

    Change label to "Short Term Debt" for consistency

--- edgar/xbrl/standardization/core.py
+++ edgar/xbrl/standardization/core.py
@@ -36,7 +36,7 @@ class StandardConcept(str, Enum):
     # Balance Sheet - Liabilities
     ACCOUNTS_PAYABLE = "Accounts Payable"
     ACCRUED_LIABILITIES = "Accrued Liabilities"
-    SHORT_TERM_DEBT = "Short-Term Debt"
+    SHORT_TERM_DEBT = "Short Term Debt"
     TOTAL_CURRENT_LIABILITIES = "Total Current Liabilities"
     LONG_TERM_DEBT = "Long Term Debt"
     DEFERRED_REVENUE = "Deferred Revenue"
