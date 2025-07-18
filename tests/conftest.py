5438587f71803cec7775e224f69bfa28300558af
paul@iqmo.com <paul@iqmo.com>
2025-07-18 16:10:27 -0400

    Disable caching

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,3 +7,11 @@ from edgar.xbrl import XBRL
 # Base paths
 FIXTURE_DIR = Path("tests/fixtures/xbrl2")
 DATA_DIR = Path("data/xbrl/datafiles")
+
+def pytest_configure(config):
+    """
+    - Disables caching for testing
+    - Uses a sqlitebucket so we can use xdist to parallelize the tests
+    """
+
+    httpclient.CACHE_DIRECTORY = None
