cdc5dbe45cddb417edcb579fa047ad30b4461eb2
paul@iqmo.com <paul@iqmo.com>
2025-07-18 16:35:23 -0400

    fix comment

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,7 +12,6 @@ DATA_DIR = Path("data/xbrl/datafiles")
 def pytest_configure(config):
     """
     - Disables caching for testing
-    - Uses a sqlitebucket so we can use xdist to parallelize the tests
     """
 
     httpclient.CACHE_DIRECTORY = None
