6adc22d268b870427784ee6392a68fdb8d36ca29
Dwight Gunning <dgunning@gmail.com>
2025-07-19 10:36:57 -0400

    Add manual download test

--- tests/manual/downloading.py
+++ tests/manual/downloading.py
@@ -7,10 +7,8 @@ use_local_storage()
 
 
 def download_filings():
-    filings = get_filings(form=['10-K', '10-Q'], filing_date='2024-04-30')
+    filings = get_filings(form=['10-K', '10-Q'], filing_date='2024-07-08')
     filings.download()
 
 if __name__ == '__main__':
-    filing = Filing(form='10-K/A', filing_date='2024-04-30', company='KORE Group Holdings, Inc.', cik=1855457, accession_no='0001855457-24-000028')
-    sgml = filing.sgml()
-    print(sgml)
+    download_filings()
