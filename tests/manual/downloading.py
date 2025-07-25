e7db9fbad64612e562e2af4bc464408bd66eb864
Dwight Gunning <dgunning@gmail.com>
2025-07-25 08:38:21 -0400

    Miscellaneous tests and gists

--- tests/manual/downloading.py
+++ tests/manual/downloading.py
@@ -6,9 +6,5 @@ os.environ['EDGAR_LOCAL_DATA_DIR'] = '/Volumes/T9/.edgar'
 use_local_storage()
 
 
-def download_filings():
-    filings = get_filings(form=['10-K', '10-Q'], filing_date='2024-07-08')
-    filings.download()
-
-if __name__ == '__main__':
-    download_filings()
+filings = get_filings(form=['10-K', '10-Q', '8-K', '6-K'], filing_date='2024-07-08')
+filings.download()
