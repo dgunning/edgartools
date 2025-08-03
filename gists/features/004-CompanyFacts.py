c3238659177c638d97eb43913ba1bff44ddfbc8b
Dwight Gunning <dgunning@gmail.com>
2025-08-03 11:22:43 -0400

    Add analysis of company facts

--- gists/features/004-CompanyFacts.py
+++ gists/features/004-CompanyFacts.py
@@ -7,3 +7,13 @@ c = Company("TSLA")
 
 facts:EntityFacts = c.get_facts()
 print(facts)
+
+income_statement = facts.income_statement()
+print(income_statement)
+
+print(facts
+      .query()
+      .by_concept("us-gaap:GrossProfit")
+      .by_period_length(12)
+      .pivot_by_period()
+)
