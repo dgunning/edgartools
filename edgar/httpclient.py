e5aa8d8912d5ffe9167841214a4fbbb7efff98c0
paul@iqmo.com <paul@iqmo.com>
2025-08-10 10:58:51 -0400

    renamed upstream project, and refactored so that edgartools maintains its own set of cache rules.

--- edgar/httpclient.py
+++ edgar/httpclient.py
@@ -1,4 +1,4 @@
-from edgar_httpx import HttpClientManager
+from httpxthrottlecache import HttpxThrottleCache
 from edgar.core import get_identity, strtobool
 import os
 from contextlib import asynccontextmanager, contextmanager
@@ -7,6 +7,19 @@ from typing import AsyncGenerator, Optional, Generator
 from pathlib import Path
 from .core import edgar_data_dir
 
+
+MAX_SUBMISSIONS_AGE_SECONDS = 10 * 60  # Check for submissions every 10 minutes
+MAX_INDEX_AGE_SECONDS = 30 * 60  # Check for updates to index (ie: daily-index) every 30 minutes
+
+CACHE_RULES = {
+    r".*\.sec\.gov": {
+        "/submissions.*": MAX_SUBMISSIONS_AGE_SECONDS,
+        r"/include/ticker\.txt.*": MAX_SUBMISSIONS_AGE_SECONDS,
+        r"/files/company_tickers\.json.*": MAX_SUBMISSIONS_AGE_SECONDS,
+        ".*index/.*": MAX_INDEX_AGE_SECONDS,
+        "/Archives/edgar/data": True,  # cache forever
+    }
+}
 
 def get_cache_directory() -> str:
     cachedir = Path(edgar_data_dir) / "_pcache"
@@ -26,14 +39,15 @@ def get_edgar_verify_ssl():
         return True
 
 
-def get_http_mgr(cache_enabled: bool = True, request_per_sec_limit: int = 9) -> HttpClientManager:
+def get_http_mgr(cache_enabled: bool = True, request_per_sec_limit: int = 9) -> HttpxThrottleCache:
     if cache_enabled:
         cache_dir = get_cache_directory()
     else:
         cache_dir = None
 
-    http_mgr = HttpClientManager(
-        user_agent=get_identity(), cache_dir=cache_dir, cache_enabled=cache_enabled, request_per_sec_limit=request_per_sec_limit
+    http_mgr = HttpxThrottleCache(
+        user_agent=get_identity(), cache_dir=cache_dir, cache_enabled=cache_enabled, request_per_sec_limit=request_per_sec_limit,
+        cache_rules = CACHE_RULES
     )
     http_mgr.httpx_params["verify"] = get_edgar_verify_ssl
     return http_mgr
