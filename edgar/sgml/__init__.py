34d3dfa3136aff53e953ab3380fc9d40914a9fc5
Dwight Gunning <dgunning@gmail.com>
2025-08-14 05:38:41 -0400

    Add Issuer, ReportingOwner, FilingMetadata to edgar.sgml __init__.py

--- edgar/sgml/__init__.py
+++ edgar/sgml/__init__.py
@@ -1,4 +1,4 @@
 from edgar.sgml.sgml_common import FilingSGML, iter_documents, list_documents
 from edgar.sgml.sgml_parser import SGMLDocument
-from edgar.sgml.sgml_header import FilingHeader, Filer
+from edgar.sgml.sgml_header import FilingHeader, Filer, Issuer, ReportingOwner, FilingMetadata
 from edgar.sgml.filing_summary import FilingSummary, Reports, Report, Statements
