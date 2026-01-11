b4933927b3f34cbb2ff570c5dfd1b3466461faa6
Dwight Gunning <dgunning@gmail.com>
2026-01-11 16:04:00 -0500

    Cleanup up files in project root

--- tests/batch/batch_financials_10Q.py
+++ tests/batch/batch_financials_10Q.py
@@ -2,13 +2,16 @@ from edgar import *
 from edgar.xbrl import XBRL
 
 def run_tenq_financials(num):
-    filings = get_filings(form='10-Q')
+    filings = get_filings(form='10-Q', year=2025)
     for filing in filings.sample(min(num, len(filings))):
         xbrl = XBRL.from_filing(filing)
         if xbrl:
             income_statement = xbrl.statements.income_statement()
             balance_sheet = xbrl.statements.balance_sheet()
             cashflow_statement = xbrl.statements.cashflow_statement()
+            print(income_statement)
+            print(balance_sheet)
+            print(cashflow_statement)
 
 
 if __name__ == '__main__':
